b2eff2e1a790b75de7e787ea4a1d37cb442d1c9e
Felipe <felipexw@gmail.com>
2016-10-26 18:33:32 -0200

    Add classe de teste.

---
+++ testing.py
@@ -0,0 +1,11 @@
+import unittest
+
+class PrimesTestCase(unittest.TestCase):
+    """Tests for `primes.py`."""
+
+    def test_is_five_prime(self):
+        """Is five successfully determined to be prime?"""
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
