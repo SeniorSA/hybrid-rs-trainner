301a0dfe9c676be417c4925f91ea70d92dc6e6e6
Felipe <felipexw@gmail.com>
2016-11-03 19:02:44 -0200

    Refactoring

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,7 +1,41 @@
 from repository.mongo_production_repository import MongoProductionRepository
+import pandas as pd
+import numpy as np
 
-def load_data():
-    client = MongoProductionRepository()
-    data_source = client.get_data_source()
+client = MongoProductionRepository()
+db = client.get_data_source()
 
-    ##theres an issue hereeee ###
+
+def __init_cf_matrix(self):
+    customers = np.array(db.clientes.distinct('codigo'))
+    items = np.array(db.produtos.distinct('codigo'))
+
+    items = np.array(self.__items)
+    items_count = len(items)
+
+    cf_matrix = pd.DataFrame(data=0 * items_count, index=customers, columns=items, dtype=float)
+
+    return cf_matrix
+
+
+def fit_cf_matrix(self, documents):
+    cf_matrix = self.__init_cf_matrix()
+
+    # veryfied
+    for target_document in documents:
+        rating = target_document.get('valorLiquido')
+        customer_code = target_document.get('cliente').get('codigo')
+        item_code = target_document.get('produto').get('codigo')
+        cf_matrix.loc[customer_code][item_code] += rating
+
+    return cf_matrix
+
+
+def __init_cf_matrix(self):
+    customers = np.array(self.__customers)
+    items = np.array(self.__items)
+    items_count = len(items)
+
+    cf_matrix = pd.DataFrame(data=0 * items_count, index=customers, columns=items, dtype=float)
+
+    return cf_matrix
--- recommender_systems/collaborative_filtering/user_user_cbf.py
+++ recommender_systems/collaborative_filtering/user_user_cbf.py
@@ -1,55 +1,35 @@
 from pymongo import MongoClient
 from os.path import join, dirname
-from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
 from collections import OrderedDict
 
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import mean_absolute_error
+
+
 class UserUserCollaborativeFiltering:
-    """ * billings args should be pass a billings matrix like:
-            - a list of dicts which should contains the keys:
-            > a str customer_code (representing the customer identifier)
-            > a str item_code (representing the item identifier)
-            > a float rating (represeting the rating which the knn based methods will use to calculate de similarity between the customers)
-                - it should look like this:
-                [{'customer_code': {'codigo': 'c1'}, 'item_code': 'i1', 'rating': 0}]
-        * customers args should be an array of str representing the customer ID
-        * items args should be an array of str representing the item ID
+    """ * the args params should be an namespace ... [to be continued]
+        * the data param should contain a pandas dataframe containing where the row (index) should be users and columns should be a item.
+            the value data.loc['user1'].values should conting  the ratings about the items
     """
 
-    def __init__(self, args, billings, customers, items):
-        self.__validate_params(args, billings, customers, items)
-        self.__billings = billings
-        self.__customers = customers
-        self.__items = items
+    def __init__(self, args, data):
+        self.__validate_params(args, data)
         self.__args = args
+        self.data = data
+        self.list_index = list(data.index)
         self.__metrics = {'accuracies': []}
 
-    def __validate_params(self, args, billings, customers, items):
-        if billings == None or billings.count() > len(customers) * len(items):
-            raise Exception('billings data source invalid')
-
-        if customers == None:
-            raise Exception('customers data source invalid')
-
-        if items == None:
-            raise Exception('items data source invalid')
+    def __validate_params(self, args, data):
+        if data == None:
+            raise Exception
 
         if args == None:
             raise Exception
 
-    def __init_cf_matrix(self):
-        customers = np.array(self.__customers)
-        items = np.array(self.__items)
-        items_count = len(items)
-
-        cf_matrix = pd.DataFrame(data=0 * items_count, index=customers, columns=items, dtype=float)
-
-        return cf_matrix
-
     def train(self):
-        billing_count = self.__billings.count()
+        billing_count = len(self.data)
 
         divider = float(billing_count) / self.__args.kfold
         accuracy = 0
@@ -57,87 +37,83 @@ class UserUserCollaborativeFiltering:
         for k in xrange(1, self.__args.kfold + 1):
             skip = int(k * divider)
             ## split the data
-            test_sample = self.__billings[:skip]
-            target_sample = self.__billings[skip + 1:]
 
-            cf_matrix = self.fit_cf_matrix(target_sample)
-            accuracy += self.__calculate_neighbors_fold(test_sample, cf_matrix)
+            test_sample = self.data.iloc[:skip]
+            cf_target_sample = self.data.iloc[skip + 1:]
 
-        accuracy = (accuracy/self.__args.kfold) * 100
+            accuracy += self.__calculate_neighbors_fold(test_sample, cf_target_sample)
 
-    def __calculate_neighbors_fold(self, test_sample, cf_matrix):
-        # verify
+        accuracy = (accuracy / self.__args.kfold) * 100
+
+    def __calculate_neighbors_fold(self, test_sample, cf_target_sample):
         accuracy = 0
-        for test_document in test_sample:
-            item_code_test = test_document.get('produto').get('codigo')
-            customer_code_test = test_document.get('cliente').get('codigo')
-            # get the item by the row, since its indexed by item
-            target_features_test = np.array([cf_matrix.loc[customer_code_test].values])
+        for test_index in test_sample.index:
+            # get the item by row, since its indexed by row
+            target_features_test = test_sample.loc[test_index].values
 
-            distances, indexes = self.__find_knn(cf_matrix, target_features_test)
+            # get the nearest neighbors
+            distances, neighbors_indexes = self.__find_knn(cf_target_sample, target_features_test)
+
             # asure that the array contains on index nearest neighbors indexes
-            indexes = [i for i in indexes[0] if self.__customers[i] != customer_code_test]
-            accuracy += self.__calculate_acuracy(indexes, customer_code_test, cf_matrix)
+            customer_index = self.list_index.index(test_index)
+            neighbors_indexes = [i for i in neighbors_indexes[0] if i != customer_index]
 
-        return accuracy/test_sample.count()
+            accuracy += self.__calculate_acuracy(neighbors_indexes, test_sample.loc[test_index], cf_target_sample)
+
+        return accuracy / test_sample.count()
 
     def __get__most_voted_items(self, indexes, cf_matrix):
         items_votes = {}
+
+        # generate the items array with zero ratings
+        for item in self.data.columns:
+            items_votes[item] = 0
+
+        # for each neighbor in neighborhood, count the items that appears in the neighbor item sample (those who was rated by a neighbor)
         for i in indexes:
-            neighbor_code = self.__customers[i]
-            neighbor = cf_matrix.loc[neighbor_code]
+            item_ratings = self.data.iloc[i]
 
-            for item in neighbor.index:
-                if neighbor[item] > 0:
-                    if item not in items_votes.keys():
-                        items_votes[item] = 1
-                    else:
-                        items_votes[item] += 1
+            for item_key in item_ratings:
+                items_votes[item_key] += item_ratings[item_key]
 
-        return self.vote(items_votes)
+        # return self.vote(items_votes, len(self.data.columns))
+        return items_votes.values()
 
-    def vote(self, items_votes):
-        #sort the most voted items
+    """ * return most voted items based on ratings
+    """
+
+    def vote(self, items_votes, threashold=None):
+        # sort the most voted items
         items_votes = OrderedDict(sorted(items_votes.items(), key=lambda x: x[1]))
+
         top_items = items_votes.keys()
 
         size = len(top_items)
-        first_index = size - self.__args.top_items
+        if threashold == None:
+            threashold = self.__args.top_items
 
-        #split the top items till top_items_threashold
+        first_index = size - threashold
+
+        # split the top items till top_items_threashold
         top_items = top_items[first_index:]
 
         top_items.reverse()
 
         return top_items
 
-    def __calculate_acuracy(self, indexes, customer_code, cf_matrix):
+    def __calculate_acuracy(self, indexes, test_rating_items, cf_matrix):
+        # get most voted items
         most_voted_items = self.__get__most_voted_items(indexes, cf_matrix)
-        test_items = set(cf_matrix.loc[customer_code].index)
 
-        hits = set(most_voted_items).intersection(test_items)
+        rmse = mean_absolute_error(test_rating_items, most_voted_items)
 
-        accuracy = len(hits)/test_items
-
-        return accuracy
+        return rmse
 
     def get_metrics(self):
         return self.__metrics
 
-    def fit_cf_matrix(self, target_sample):
-        cf_matrix = self.__init_cf_matrix()
-
-        # veryfied
-        for target_document in target_sample:
-            rating = target_document.get('valorLiquido')
-            customer_code = target_document.get('cliente').get('codigo')
-            item_code = target_document.get('produto').get('codigo')
-            cf_matrix.loc[customer_code][item_code] += rating
-
-        return cf_matrix
-
-    def __find_knn(self, cf_matrix, target_features):
+    def __find_knn__find_knn(self, target_matrix, target_features):
         neighbors = NearestNeighbors(n_neighbors=self.__args.n_neighbors, algorithm=self.__args.alg).fit(
-            cf_matrix.values)
+            target_matrix.values)
         distances, indexes = neighbors.kneighbors(target_features)
         return distances, indexes
