bbbf51492a09921ea56fce3d9febcddc5bec0734
Felipe <felipexw@gmail.com>
2016-11-04 14:21:29 -0200

    Implemented get_items_code in produto_repository_mongo

--- repository/produto_repository_mongo.py
+++ repository/produto_repository_mongo.py
@@ -7,7 +7,7 @@ class ProdutoRepositoryMongo(ProdutoRepository, GenericMongoRepository):
     def __init__(self, repository):
         self.repository = repository
 
-    def get_customers_code(self):
+    def get_items_code(self):
         db = self.repository.get_data_source()
         item_colls = db[self.repository.args.item_collection_name]
         customer_codes = item_colls.distinct('codigo')
