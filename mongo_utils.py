fbb167658c9a3c61bc43eb50ec1253c4a3ce5e2f
Felipe Appio <felipexw@gmail.com>
2016-11-03 23:50:34 -0200

    Refactoring.

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,16 +1,17 @@
 from repository.mongo_production_repository import MongoProductionRepository
+from repository.faturamento_repository import FaturamentoRepository
+from repository.cliente_repository import  ClienteRepository
+from repository.produto_repository import ProdutoRepository
+from repository.generic_repository import GenericRepository
 import pandas as pd
 import numpy as np
 
-client = MongoProductionRepository()
-db = client.get_data_source()
+def init_cf_matrix(args):
+    customer_repository = ClienteRepository(MongoProductionRepository(args=args))
+    customers = np.array(customer_repository.get_customers_code())
 
-
-def __init_cf_matrix(self):
-    customers = np.array(db.clientes.distinct('codigo'))
-    items = np.array(db.produtos.distinct('codigo'))
-
-    items = np.array(self.__items)
+    item_repository = ProdutoRepository(MongoProductionRepository(args=args))
+    items = np.array(item_repository.get_items_code())
     items_count = len(items)
 
     cf_matrix = pd.DataFrame(data=0 * items_count, index=customers, columns=items, dtype=float)
@@ -18,24 +19,15 @@ def __init_cf_matrix(self):
     return cf_matrix
 
 
-def fit_cf_matrix(self, documents):
-    cf_matrix = self.__init_cf_matrix()
+def load_data(args):
+    billings = FaturamentoRepository(MongoProductionRepository(args))
+    documents = billings.find()
+    cf_matrix = init_cf_matrix()
 
-    # veryfied
     for target_document in documents:
         rating = target_document.get('valorLiquido')
         customer_code = target_document.get('cliente').get('codigo')
         item_code = target_document.get('produto').get('codigo')
         cf_matrix.loc[customer_code][item_code] += rating
-
-    return cf_matrix
-
-
-def __init_cf_matrix(self):
-    customers = np.array(self.__customers)
-    items = np.array(self.__items)
-    items_count = len(items)
-
-    cf_matrix = pd.DataFrame(data=0 * items_count, index=customers, columns=items, dtype=float)
 
     return cf_matrix
