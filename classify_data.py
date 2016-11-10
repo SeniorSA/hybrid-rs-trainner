2aa601a638d5a91a044c0e6d4cac75de207e637b
Felipe <felipexw@gmail.com>
2016-11-10 18:35:03 -0200

    classify_data script updates.

--- classify_data.py
+++ classify_data.py
@@ -1,16 +1,20 @@
 import argparse
 from sklearn.externals import joblib
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
 
-from repository.cliente_repository_mongo import ClienteRepositoryMongo
-from repository.produto_repository_mongo import ProdutoRepositoryMongo, ProdutoRepository
-from repository.faturamento_repository_mongo import FaturamentoRepositoryMongo
+from repository.cliente_repository import ClienteRepositoryMongo
+from repository.produto_repository import ProdutoRepositoryMongo
+from repository.faturamento_repository import FaturamentoRepositoryMongo
+from repository.oportunidade_repository import OportunidadeRepositoryMongo
 from repository.mongo_production_repository import MongoProductionRepository
 from repository.mongo_repository import GenericMongoRepository
-from mongo_utils import load_data
 
 parser = argparse.ArgumentParser(description='Calculate eSocial Campaigns')
 
-parser.add_argument('--pickle-filename', help='specify the pickled filename relative to "classify_data.py"', type=str)
+parser.add_argument('--pickle-filename', help='specify the pickled filename relative to "classify_data.py"', type=str,
+                    default='user_user_cf_knn-10-11-2016-9-27-0.pkl')
 parser.add_argument('--mongo-database-url', help='mongo database url', default='localhost')
 parser.add_argument('--mongo-database-name', help='mongo database name', default='testando')
 parser.add_argument('--customer-collection-name', help='mongo customer collection name', default='clientes')
@@ -20,299 +24,56 @@ parser.add_argument('--opportunity-collection-name', help='mongo opportunity col
 
 args = parser.parse_args()
 
-def load_data():
-    item_repository = ProdutoRepositoryMongo(MongoProductionRepository(args), args.item_collection_name)
-    billing_repository = FaturamentoRepositoryMongo(MongoProductionRepository(args), args.billing_collection_name)
-    customer_repository = ClienteRepositoryMongo(MongoProductionRepository(args), args.customer_collection_name)
-
-    cf_matrix = load_data(customer_repository, item_repository, billing_repository)
-
-    return cf_matrix
-
-
 user_user_cf = joblib.load(args.pickle_filename)
 
+target_items = ['S-1-100201', 'S-1-090235']
+db = MongoClient()['testando']
+
+item_repository = ProdutoRepositoryMongo(MongoProductionRepository(args), args.item_collection_name)
+
+columns = item_repository.get_items_code()
 
 
-# items = user_user_cf.cf_matrix.columns
-#
-# clientes = [
-#     "41",
-#     "50",
-#     "78",
-#     "83",
-#     "152",
-#     "193",
-#     "195",
-#     "206",
-#     "281",
-#     "291",
-#     "343",
-#     "350",
-#     "364",
-#     "368",
-#     "373",
-#     "446",
-#     "461",
-#     "521",
-#     "538",
-#     "586",
-#     "609",
-#     "612",
-#     "626",
-#     "636",
-#     "647",
-#     "672",
-#     "682",
-#     "713",
-#     "787",
-#     "822",
-#     "837",
-#     "1066",
-#     "1109",
-#     "1319",
-#     "1340",
-#     "1837",
-#     "1839",
-#     "1847",
-#     "1863",
-#     "1906",
-#     "1947",
-#     "1975",
-#     "2026",
-#     "2030",
-#     "2033",
-#     "2710",
-#     "2815",
-#     "2879",
-#     "2881",
-#     "2906",
-#     "2930",
-#     "3122",
-#     "3127",
-#     "3128",
-#     "3173",
-#     "3188",
-#     "3250",
-#     "3395",
-#     "3444",
-#     "3649",
-#     "3706",
-#     "3798",
-#     "3832",
-#     "4165",
-#     "4196",
-#     "4615",
-#     "4720",
-#     "4842",
-#     "5121",
-#     "5123",
-#     "5193",
-#     "5205",
-#     "5285",
-#     "5469",
-#     "5485",
-#     "5820",
-#     "6017",
-#     "6342",
-#     "6839",
-#     "6930",
-#     "7074",
-#     "7077",
-#     "7227",
-#     "7303",
-#     "7474",
-#     "7692",
-#     "7728",
-#     "7858",
-#     "7971",
-#     "8171",
-#     "9516",
-#     "9761",
-#     "9784",
-#     "9839",
-#     "9880",
-#     "10038",
-#     "10107",
-#     "10154",
-#     "10219",
-#     "10331",
-#     "11059",
-#     "11168",
-#     "11191",
-#     "11271",
-#     "11284",
-#     "11306",
-#     "11338",
-#     "11489",
-#     "11500",
-#     "11568",
-#     "11595",
-#     "11599",
-#     "11777",
-#     "11862",
-#     "11937",
-#     "11967",
-#     "12008",
-#     "12026",
-#     "12070",
-#     "12228",
-#     "12328",
-#     "12374",
-#     "12423",
-#     "12556",
-#     "12579",
-#     "12581",
-#     "12604",
-#     "12896",
-#     "12910",
-#     "13075",
-#     "13217",
-#     "13243",
-#     "13251",
-#     "13353",
-#     "13527",
-#     "13618",
-#     "13682",
-#     "13710",
-#     "13772",
-#     "13799",
-#     "13909",
-#     "13954",
-#     "14069",
-#     "14102",
-#     "14305",
-#     "14312",
-#     "14358",
-#     "14386",
-#     "14425",
-#     "14513",
-#     "14514",
-#     "14576",
-#     "14590",
-#     "14613",
-#     "14649",
-#     "14659",
-#     "14726",
-#     "14848",
-#     "14958",
-#     "14990",
-#     "15020",
-#     "15059",
-#     "15077",
-#     "15092",
-#     "15114",
-#     "15254",
-#     "15278",
-#     "15418",
-#     "15427",
-#     "15435",
-#     "15524",
-#     "15570",
-#     "15638",
-#     "15948",
-#     "16011",
-#     "16046",
-#     "16102",
-#     "16138",
-#     "16175",
-#     "16227",
-#     "16425",
-#     "16452",
-#     "16602",
-#     "16642",
-#     "16698",
-#     "16724",
-#     "16735",
-#     "16891",
-#     "16913",
-#     "16925",
-#     "16985",
-#     "16998",
-#     "17054",
-#     "17065",
-#     "17079",
-#     "17144",
-#     "17157",
-#     "17182",
-#     "17246",
-#     "17283",
-#     "17293",
-#     "17338",
-#     "17390",
-#     "17444",
-#     "17520",
-#     "17583",
-#     "17636",
-#     "17682",
-#     "17685",
-#     "17712",
-#     "17715",
-#     "17812",
-#     "17848",
-#     "17875",
-#     "17969",
-#     "18101",
-#     "18135",
-#     "18200",
-#     "18202",
-#     "18221",
-#     "18301",
-#     "18327",
-#     "18373",
-#     "18470",
-#     "18615",
-#     "18788",
-#     "18875",
-#     "18892",
-#     "18952",
-#     "19023",
-#     "19129",
-#     "19194",
-#     "19229",
-#     "19413",
-#     "19479",
-#     "19952"
-# ]
-#
-# import matplotlib.pyplot as plt
-#
-# from pymongo import MongoClient
-#
-# db = MongoClient()['testando']
-# rows = user_user_cf.cf_matrix.index[:]
-# items = user_user_cf.cf_matrix.columns
-#
-# ratings = user_user_cf.cf_matrix[:]['S-1-100201']
-# for i in ratings:
-#     if i > 0:
-#         print 'i > 0'
-#
-# ratings = user_user_cf.cf_matrix[:]['S-1-090235']
-# for i in ratings:
-#     if i > 0:
-#         print 'i > 0'  #
-#
-# # for cliente in clientes:
-# #     if cliente not in rows:
-# #         clientes.remove(cliente)
-# #
-# # features = {}
-# # for i in items:
-# #     features[i] = 0
-# #
-#
-# count = 0
-# for cliente in clientes:
-#     if cliente in rows:
-#         target = user_user_cf.cf_matrix.loc[cliente].values
-#         predictions = user_user_cf.predict(target)
-#
-#         for i in xrange(len(predictions)):
-#             recommendation = {'buy': target[i], 'codigoProduto': items[i], 'codigoCliente': cliente}
-#             db.knnPredictions.save(recommendation)
-#
-#     else:
-#         count += 1
-#
-# print count
+def load_data(cf_matrix):
+    billing_repository = FaturamentoRepositoryMongo(MongoProductionRepository(args), args.billing_collection_name)
+    count = billing_repository.count()
+
+    for customer_code in cf_matrix.index:
+        documents = billing_repository.find(0, count, {'cliente.codigo': customer_code})
+
+        for doc in documents:
+            item_code = doc.get('produto').get('codigo')
+            cf_matrix.loc[customer_code][item_code] = 1
+
+
+def init_matrix():
+    opportunity_repository = OportunidadeRepositoryMongo(MongoProductionRepository(args),
+                                                         args.opportunity_collection_name)
+    customers = opportunity_repository.get_customers_code()
+
+    data = []
+
+    for customer in customers:
+        target_features = [i * 0 for i in xrange(len(columns))]
+        data.append(target_features)
+
+    test_matrix = pd.DataFrame(data=data, index=customers, columns=columns, dtype=int)
+
+    load_data(test_matrix)
+
+    return test_matrix
+
+
+def save_predictions(test_matrix):
+    for customer_code in test_matrix.index:
+        target_features = test_matrix.loc[customer_code].values
+        predictions = user_user_cf.predict(target_features)
+
+        for i in xrange(len(predictions)):
+            recommendation = {'buy': predictions[i], 'codigoProduto': columns[i], 'codigoCliente': customer_code,
+                              'version': args.pickle_filename}
+            db.knnPredictions.save(recommendation)
+
+
+test_matrix = init_matrix()
+save_predictions(test_matrix)
